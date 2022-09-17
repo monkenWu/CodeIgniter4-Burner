39d5e21aade529e3057be23a47eb1d84d60d29bb
monkenWu <monkenWu@monken.tw>
2022-09-18 00:53:38 +0800

    20220918 modify worker init path to real path

--- src/RoadRunner/Worker.php
+++ src/RoadRunner/Worker.php
@@ -1,13 +1,13 @@
 <?php
-
+$pathsConfig = '';
 if (file_exists(__DIR__ . DIRECTORY_SEPARATOR . '../../../../autoload.php')) {
     require_once realpath(__DIR__ . DIRECTORY_SEPARATOR . '../../../../autoload.php');
-    define('FCPATH', __DIR__ . DIRECTORY_SEPARATOR);
+    if (!defined('FCPATH')) define('FCPATH', __DIR__ . DIRECTORY_SEPARATOR);
     chdir(FCPATH);
     $pathsConfig = realpath(FCPATH . '../../../../../app/Config/Paths.php');
-} elseif (file_exists('../../dev/vendor/autoload.php')) {
-    require_once '../../dev/vendor/autoload.php';
-    define('FCPATH', __DIR__ . DIRECTORY_SEPARATOR);
+} elseif (file_exists(__DIR__ . DIRECTORY_SEPARATOR . '../../dev/vendor/autoload.php')) {
+    require_once __DIR__ . DIRECTORY_SEPARATOR . '../../dev/vendor/autoload.php';
+    if (!defined('FCPATH'))  define('FCPATH', __DIR__ . DIRECTORY_SEPARATOR);
     chdir(FCPATH);
     $pathsConfig = realpath(FCPATH . '../../dev/app/Config/Paths.php');
 }
@@ -32,10 +32,10 @@ if (! function_exists('is_cli')) {
 /**
  * Ci4 4.2.0 init
  */
-require realpath($pathsConfig) ?: $pathsConfig;
+require_once realpath($pathsConfig) ?: $pathsConfig;
 $paths     = new Paths();
 $botstorap = rtrim($paths->systemDirectory, '\\/ ') . DIRECTORY_SEPARATOR . 'bootstrap.php';
-require realpath($botstorap);
+require_once realpath($botstorap);
 require_once SYSTEMPATH . 'Config/DotEnv.php';
 (new \CodeIgniter\Config\DotEnv(ROOTPATH))->load();
 $app = \Config\Services::codeigniter();

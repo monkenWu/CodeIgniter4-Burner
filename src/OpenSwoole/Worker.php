e9d949741999e555b49df5a2fa79e47b5bb04d12
monkenWu <monkenWu@monken.tw>
2022-10-26 00:18:48 +0800

    2022.10.25 added response event

--- src/OpenSwoole/Worker.php
+++ src/OpenSwoole/Worker.php
@@ -7,26 +7,45 @@ require_once realpath(__DIR__ . '/../FrontLoader.php');
 define('BURNER_DRIVER', 'OpenSwoole');
 
 use CodeIgniter\Config\Factories;
+use \CodeIgniter\Events\Events;
 use Imefisto\PsrSwoole\ResponseMerger;
 use Imefisto\PsrSwoole\ServerRequest as PsrRequest;
 use Nyholm\Psr7\Factory\Psr17Factory;
 use Swoole\Http\Request;
 use Swoole\Http\Response;
+use Swoole\Http\Server;
 
 class Worker
 {
-    protected static $uriFactory;
-    protected static $streamFactory;
-    protected static $uploadedFileFactory;
-    protected static $responseMerger;
-    private static $init = false;
+    protected static Psr17Factory $uriFactory;
+    protected static Psr17Factory $streamFactory;
+    protected static Psr17Factory $uploadedFileFactory;
+    protected static ResponseMerger $responseMerger;
+    protected static \Swoole\HTTP\Server|\Swoole\WebSocket\Server $server;
 
-    protected static function init()
+    /**
+     * Init Worker
+     *
+     * @param \Swoole\HTTP\Server|\Swoole\WebSocket\Server $server
+     * @return void
+     */
+    public static function init(\Swoole\HTTP\Server|\Swoole\WebSocket\Server $server)
     {
         self::$uriFactory          = new Psr17Factory();
         self::$streamFactory       = new Psr17Factory();
         self::$uploadedFileFactory = new Psr17Factory();
         self::$responseMerger      = new ResponseMerger();
+        self::$server = $server;
+    }
+
+    /**
+     * get OpenSwoole Server Instance
+     *
+     * @return \Swoole\HTTP\Server|\Swoole\WebSocket\Server
+     */
+    public static function getServer(): \Swoole\HTTP\Server|\Swoole\WebSocket\Server
+    {
+        return self::$server;
     }
 
     /**
@@ -37,9 +56,6 @@ class Worker
      */
     public static function mainProcesser(Request $swooleRequest, Response $swooleResponse)
     {
-        if (self::$init === false) {
-            self::init();
-        }
         if (null === $swooleRequest->files) {
             $swooleRequest->files = [];
         }
@@ -57,6 +73,8 @@ class Worker
             $response,
             $swooleResponse
         )->end();
+        
+        Events::trigger('burnerAfterSendResponse', self::$server);
 
         \Monken\CIBurner\App::clean();
     }
@@ -70,6 +88,7 @@ $server           = new ($openSwooleConfig->httpDriver)(
     $openSwooleConfig->mode,
     $openSwooleConfig->type
 );
+Worker::init($server);
 $server->set($openSwooleConfig->config);
 $openSwooleConfig->server($server);
 $server->start();
